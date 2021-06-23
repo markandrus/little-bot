9b07f9c7fd52efb63e307b1d8a50a8bf41b41204
Mark Andrus Roberts <markandrusroberts@gmail.com>
2021-06-23 20:30:58 +0200

    Add support for multiple URLs

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
+require('dotenv').config();
+
+const {
+  TWILIO_PHONE_NUMBER_TO_1,
+  TWILIO_PHONE_NUMBER_TO_2,
+  URL_1,
+  URL_2
+} = process.env;
+
 const {
   error,
   loadPage
 } = require('../lib');
 
 async function main() {
-  await loadPage();
+  await loadPage(URL_1, TWILIO_PHONE_NUMBER_TO_1);
+  await loadPage(URL_2, TWILIO_PHONE_NUMBER_TO_2);
 }
 
 main().catch(({ stack }) => {
